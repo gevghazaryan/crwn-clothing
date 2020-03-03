409701fa61538e4c13698e4f9dbbc8d1a913b574
gev <gevghazaryan>
2020-03-04 00:08:51 +0400

    Addint state management for carticon hidden and show effects using react redux

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';  
 import { auth, createUserProfileDocument } from './firebase/firebase.util';
 import { setCurrentUser } from './redux/user/user.actions';
@@ -49,15 +49,19 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={ HomePage } />
           <Route path="/shop" component={ ShopPage } />      
-          <Route path="/signin" component={ SigninAndSingupPage } />        
-        </Switch>
+          <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SigninAndSingupPage />)  } />
+        </Switch> 
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,8 +6,10 @@ import { connect } from 'react-redux';
 
 import "./header.style.scss";
 import { ReactComponent as Logo } from "../../assets/071 crown.svg";
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser,hidden }) => (
     <div className="header">
         <Link className="logo-container" to="/">
              <Logo className="logo" />
@@ -21,13 +23,18 @@ const Header = ({ currentUser }) => (
                     :
                     <Link className="option" to="/signin">SING IN </Link>
                 }
+        <CartIcon />
         </div>
-
+        {
+            hidden ? null :
+            <CartDropDown />
+        }
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
