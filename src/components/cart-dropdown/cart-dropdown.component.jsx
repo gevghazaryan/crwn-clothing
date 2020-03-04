410d40de5d3a5d25e98c877897fe1ac8e129bf51
gev <gevghazaryan>
2020-03-04 13:10:21 +0400

    added shop's cart functionality, now user can see his purchases and amount of items he bought

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+
+import {connect} from 'react-redux';
 
 import './cart-dropdown.style.scss';
 
 
-const CartDropDown = () => (
+const CartDropDown = ({cartItems}) => (
     <div className="cart-dropdown">
-        <div className="cart-item"></div>
+            {cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item={cartItem} />
+            ))}
+
         <CustomButton>Go To CHECKOUT</CustomButton>
     </div>
 )
 
-export default CartDropDown
+const mapStateToProps = ({ cart: {cartItems}}) => ({
+    cartItems
+})
+
+export default connect(mapStateToProps)(CartDropDown) 
--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import {connect} from 'react-redux';
+
+import {addItem} from '../../redux/cart/cart.actions';
+import CustomButton from '../custom-button/custom-button.component';
+
 import "./collection-item.style.scss";
 
-const CollectionItem = ({id,price,imageUrl,name}) => 
-    (
+const CollectionItem = ({item,addItem}) => {    
+    const {imageUrl,name,price} = item
+    return (
         <div className="collection-item">
             <div
              className="image"
@@ -13,7 +19,13 @@ const CollectionItem = ({id,price,imageUrl,name}) =>
                 <span className="item-name">{name}</span>
                 <span className="price">{price}</span>                
             </div>
+            <CustomButton inverted onClick={() => addItem(item)}>Add To Cart</CustomButton>
         </div>
     )
+}
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
